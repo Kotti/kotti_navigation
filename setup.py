971c66686b0c6eaa46edd9c029977d7bab472c0d
Marco Scheidhuber <marco@jusid.de>
2012-05-03 15:09:15 +0200

    Update the defaults a little bit.

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
+
 version = '0.1'
 project = 'kotti_navigation'
 
@@ -16,20 +17,22 @@ tests_require = [
 
 setup(name=project,
       version=version,
-      description="",
+      description="Navigation widget for Kotti",
       long_description="""\
+This is an extension to the Kotti CMS that renders a navigation in the
+left or right slot.
 """,
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='',
+      keywords='kotti addon',
       author='Marco Scheidhuber',
       author_email='marco@jusid.de',
-      url='',
+      url='http://jusid.de',
       license='GPL',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'Kotti',
+          'Kotti>=0.6.2',
       ],
       tests_require=tests_require,
       entry_points="""
