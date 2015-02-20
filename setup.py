ad0957cc2c1c96f8b2b8b66d57eb26cd92463bde
Marco Scheidhuber <j23d@jusid.de>
2015-02-20 20:17:57 +0100

    Bump version.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ long_description = (
 
 
 setup(name='kotti_navigation',
-      version='0.3.1',
+      version='0.3.2',
       description="""Add a configurable navigation to your Kotti site""",
       long_description=long_description,
       classifiers=[
