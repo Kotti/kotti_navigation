e0da6bbd96064349c651427c038b9a919775d5cf
Marco Scheidhuber <j23d@jusid.de>
2016-02-19 12:10:25 +0100

    Move part of the browser tests to webtests.

--- kotti_navigation/tests/conftest.py
+++ kotti_navigation/tests/conftest.py
@@ -1,6 +1,55 @@
 from pytest import fixture
+from pytest import mark
+
+from kotti.views.slots import assign_slot
 
 pytest_plugins = "kotti"
+
+
+@fixture(scope='session')
+def custom_settings():
+    return {
+        'kotti.configurators': 'kotti_navigation.kotti_configure'
+        }
+
+@fixture
+def kn_populate(db_session):
+    from kotti_navigation.populate import populate
+    populate()
+
+
+@mark.user('admin')
+@fixture
+def kn_content(webtest, kn_populate, root, workflow):
+
+    from kotti.workflow import get_workflow
+
+    resp = webtest.get('/add_document')
+    form = resp.forms['deform']
+    form['title'].value = 'Document 1'
+    form['description'].value = 'This is the first document'
+    resp = form.submit('save')
+
+    resp = webtest.get('/document-1/add_document')
+    form = resp.forms['deform']
+    form['title'].value = 'Document 1 1'
+    form['description'].value = 'This is the second document'
+    resp = form.submit('save')
+
+    wf = get_workflow(root['document-1']);
+    wf.transition_to_state(root['document-1'], None, u'public')
+    wf = get_workflow(root['document-1']['document-1-1']);
+    wf.transition_to_state(root['document-1']['document-1-1'], None, u'public')
+
+
+@fixture
+def assign_left():
+    assign_slot('navigation-widget', 'left')
+
+
+@fixture
+def assign_right():
+    assign_slot('navigation-widget', 'right')
 
 
 def settings():
@@ -9,22 +58,18 @@ def settings():
     settings = conf_defaults.copy()
     settings['kotti.secret'] = 'secret'
     settings['kotti.secret2'] = 'secret2'
-    settings['kotti.configurators'] = \
-        'kotti_navigation.kotti_configure kotti_settings.kotti_configure'
-    settings['kotti.populators'] = 'kotti.testing._populator'
-    _resolve_dotted(settings)
+    settings['kotti.configurators'] += \
+        ' kotti_settings.kotti_configure kotti_navigation.kotti_configure'
+    settings['kotti.populators'] += ' kotti_navigation.populate.populate kotti_navigation.tests._populate_left'
+    settings['pyramid.includes'] += ' kotti_settings kotti_settings.views'
+    settings = _resolve_dotted(settings)
     return settings
 
 
 def setup_app():
     from kotti import base_configure
+    import ipdb;ipdb.set_trace()
     return base_configure({}, **settings()).make_wsgi_app()
-
-
-@fixture
-def kn_populate(db_session):
-    from kotti_navigation.populate import populate
-    populate()
 
 
 @fixture
---
+++ kotti_navigation/tests/test_options.py
@@ -0,0 +1,67 @@
+from pytest import mark
+
+from kotti_navigation.tests import set_nav_setting
+
+
+def dc(content, name='/tmp/out.html'):
+    open(name, 'w').write(content)
+
+
+class TestOptions:
+
+    @mark.user('admin')
+    def test_open_all(self, webtest, kn_content, assign_left):
+        set_nav_setting('left', 'show_in_context', 'everywhere')
+        set_nav_setting('left', 'display_type', 'tree')
+        resp = webtest.get('/')
+        assert 'Document 1 1' not in resp.body
+
+        set_nav_setting('left', 'options', ['open_all'])
+        resp = webtest.get('/')
+        assert 'Document 1 1' in resp.body
+
+    @mark.user('admin')
+    def test_hidden_nav_points(self, webtest, kn_content, assign_left):
+        set_nav_setting('left', 'options', ['open_all'])
+        set_nav_setting('left', 'show_in_context', 'everywhere')
+        set_nav_setting('left', 'display_type', 'tree')
+
+        resp = webtest.get('/')
+        assert 'Document 1 1' in resp.body
+
+        resp = webtest.get('/document-1/@@contents')
+        form = resp.forms[1]
+        form.set('children', True)
+        resp = form.submit(name='hide').maybe_follow()
+        assert 'Document 1 1 is no longer visible' in resp.text
+
+        resp = webtest.get('/')
+        assert 'Document 1 1' not in resp.body
+
+        set_nav_setting('left', 'options', ['open_all', 'show_hidden_while_logged_in'])
+        resp = webtest.get('/')
+        assert 'Document 1 1' in resp.body
+
+    @mark.user('admin')
+    def test_include_root(self, webtest, kn_content, assign_left):
+        set_nav_setting('left', 'show_in_context', 'everywhere')
+        set_nav_setting('left', 'display_type', 'tree')
+
+        resp = webtest.get('/')
+        assert 'title="Congratulations! ' not in resp.text
+
+        set_nav_setting('left', 'options', ['include_root'])
+        resp = webtest.get('/')
+        assert 'title="Congratulations! ' in resp.text
+
+    @mark.user('admin')
+    def test_stacked(self, webtest, kn_content, assign_left):
+        set_nav_setting('left', 'show_in_context', 'everywhere')
+        set_nav_setting('left', 'display_type', 'tree')
+
+        resp = webtest.get('/')
+        assert '<ul class="nav nav-pills nav-stacked">' not in resp.text
+
+        set_nav_setting('left', 'options', ['stacked'])
+        resp = webtest.get('/')
+        assert '<ul class="nav nav-pills nav-stacked">' in resp.text
